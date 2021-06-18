9b2be8d1d4bf1d0ff6b35da1cd442ad2d0aa3d18
Administrator <admin@example.com>
2021-06-18 15:46:05 +0430

    set right path

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='vsdkx-model-bayesian',
-    url='https://gitlab.com/natix/visiondeploy/aiconnector',
+    url='https://gitlab.com/natix/cvison/vsdkx/vsdkx-model-bayesian',
     author='Helmut',
     author_email='helmut@natix.io',
     namespace_packages=['vsdkx', 'vsdkx.model'],
